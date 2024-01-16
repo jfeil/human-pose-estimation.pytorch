e6c5d5b486b0c188e1c401d343729162abf48cba
Jan Feil <github@jfeil.de>
2024-01-16 10:35:17 +0100

    Add visualization function

---
+++ lib/utils/visualization.py
@@ -0,0 +1,49 @@
+import cv2
+from copy import copy
+
+"""                
+    "Shoulder Left",
+    "Shoulder Right",
+    "Elbow Left",
+    "Hand Left",
+    "Elbow Right",
+    "Hand Right",
+    "Foot Left",
+    "Foot Right",
+    "Hip",
+    "Neck"
+"""
+
+left_color = (255, 0, 219)
+center_color = (0, 228, 255)
+right_color = (0, 0, 255)
+
+colors = [
+    left_color,
+    right_color,
+    left_color,
+    left_color,
+    right_color,
+    right_color,
+    left_color,
+    right_color,
+    center_color,
+    center_color
+]
+
+def draw_skeleton(frame, pred, skeleton=[[0,2],[9,8],[8,6],[9,1],[4,5],[9,0],[2,3],[1,4],[8,7]]):
+    frame = copy(frame)
+    centers = [(int(point[0]), int(point[1])) for point in pred]
+    for i, point in enumerate(centers):
+        frame = cv2.circle(frame, point, 2, colors[i])
+    for p1, p2 in skeleton:
+        frame = cv2.line(frame, centers[p1], centers[p2], 0)
+    return frame
+
+def draw_rectangle(frame, pred):
+    x, y, w, h, = pred
+    start_point = (int(x - w//2), int(y - h//2))
+    end_point = (int(x + w//2), int(y + h//2))
+    # start_point = (10, 10)
+    # end_point = (200, 200)
+    return cv2.rectangle(frame, start_point, end_point, 1, 2) 
